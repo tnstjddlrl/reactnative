ca4857336688006f0b4a4f9567c1625cd97aff8a
DESKTOP-V0TP74J\user <tnstjddlrl@naver.com>
2021-01-11 12:19:34 +0900

    일단 견적테이블에 셀렉트메뉴 구현완료. 밑에 테이블만 구현하면 끝

--- currentTable.js
+++ currentTable.js
@@ -5,7 +5,8 @@ import {
   ImageBackground,
   Dimensions,
   TouchableOpacity,
-  Image
+  Image,
+  Modal
 } from 'react-native';
 
 const chartHeight = Dimensions.get('window').height;
@@ -19,8 +20,13 @@ import { useNavigation } from '@react-navigation/native';
 import FootTer from './footer.js'
 import HeadHeder from "./header.js";
 import { ScrollView } from 'react-native-gesture-handler';
+import { useState } from 'react/cjs/react.development';
 
 const CurrentTable =() =>{
+    const navigation = useNavigation();
+    const [select, setSelect] = useState(false)
+    const [listCate,SetlistCate] = useState('전체')
+
     return(
         <View>
             <View style={{width:chartWidth,height:chartHeight}}>
@@ -31,14 +37,61 @@ const CurrentTable =() =>{
                     <Text style={{position:'absolute',color:"white",fontSize:20,fontWeight:'bold',top:40,left:10}}>견적현황</Text>
                     </View>
 
-                    
+
+                    <View style={{flexDirection:'row'}}>
+                        <TouchableOpacity onPress={()=>setSelect(true)}>
+                            <View style={{width:100,height:30,borderWidth:0.7,left:10,marginTop:20,alignItems:"center",flexDirection:"row"}}>
+                                <Text style={{left:2}}>{listCate}</Text>
+                                <Image source={arrow} style={{width:8,height:14,right:5,position:'absolute'}}></Image>
+                            </View>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={()=>navigation.navigate('견적의뢰')}>
+                            <View style={{backgroundColor:'#A579C9',width:70,height:30,left:10,marginTop:20,alignItems:'center',alignContent:'center'}}>
+                                <Text style={{color:'white',marginTop:5,fontWeight:"800"}}>견적의뢰</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+
 
 
 
                 </ScrollView>
+
+                <Modal  transparent={true} visible={select}>
+                    <View style={{width:100, height:180,position:'absolute',backgroundColor:'white',borderWidth:0.5,left:10,top:150}}>
+                        <TouchableOpacity onPress={()=>{SetlistCate('전체'),setSelect(false)}}>
+                        <Text style={{left:5,marginTop:5}}>전체</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={()=>{SetlistCate('입찰대기'),setSelect(false)}}>
+                        <Text style={{left:5,marginTop:5}}>입찰대기</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={()=>{SetlistCate('입찰진행중'),setSelect(false)}}>
+                        <Text style={{left:5,marginTop:5}}>입찰진행중</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={()=>{SetlistCate('시공진행중'),setSelect(false)}}>
+                        <Text style={{left:5,marginTop:5}}>시공진행중</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={()=>{SetlistCate('시공완료'),setSelect(false)}}>
+                        <Text style={{left:5,marginTop:5}}>시공완료</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={()=>{SetlistCate('시공취소'),setSelect(false)}}>
+                        <Text style={{left:5,marginTop:5}}>시공취소</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={()=>{SetlistCate('견적취소'),setSelect(false)}}>
+                        <Text style={{left:5,marginTop:5}}>견적취소</Text>
+                        </TouchableOpacity>
+                    </View>
+                </Modal>
             </View>
+            
+            
+
+
+            
             <HeadHeder></HeadHeder>
                 <FootTer></FootTer>
+
+            
 
         </View>
     )
